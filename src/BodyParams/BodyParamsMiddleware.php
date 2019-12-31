b3c66ddae14dc43e05f80e02e31ab816a082daaa
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:44:43 -0600

    Rewriting as Laminas Project package

--- src/BodyParams/BodyParamsMiddleware.php
+++ src/BodyParams/BodyParamsMiddleware.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-helpers for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-helpers/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-helpers for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-helpers/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-helpers/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Expressive\Helper\BodyParams;
+namespace Mezzio\Helper\BodyParams;
 
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
