eebf3a958bf187d8ccb802bf4624ebe6a6bc8bae
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:44:38 -0600

    Rewriting as Laminas Project package

--- test/BodyParams/BodyParamsMiddlewareTest.php
+++ test/BodyParams/BodyParamsMiddlewareTest.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-helpers for the canonical source repository
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-helpers/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-helpers for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-helpers/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-helpers/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Expressive\Helper\BodyParams;
+namespace MezzioTest\Helper\BodyParams;
 
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Stream;
+use Mezzio\Helper\BodyParams\BodyParamsMiddleware;
+use Mezzio\Helper\BodyParams\StrategyInterface;
+use Mezzio\Helper\Exception\MalformedRequestBodyException;
 use PHPUnit_Framework_TestCase as TestCase;
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Stream;
-use Zend\Expressive\Helper\BodyParams\BodyParamsMiddleware;
-use Zend\Expressive\Helper\BodyParams\StrategyInterface;
-use Zend\Expressive\Helper\Exception\MalformedRequestBodyException;
 
 class BodyParamsMiddlewareTest extends TestCase
 {
